7dc443ea6c35f0f02a008186d3e07756c1f84c1e
CheersXRay <cheersemail@gmail.com>
2026-05-15 13:45:52 +0800

    update public test.js

---
+++ public/js/test.js
@@ -0,0 +1,4 @@
+console.log("----------------------------");
+console.log("✅ Node.js is working!");
+console.log("Current Version: " + process.version);
+console.log("----------------------------");
